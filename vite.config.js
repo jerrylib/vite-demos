1d769f077d5d7360c2237097468e1004998eed7c
韩旭gogogo <js_bin@163.com>
2024-06-11 19:19:10 +0800

    chore: alias config add

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from "vite";
+import * as path from 'path'
+
+// === Plugins === //
 import react from "@vitejs/plugin-react-swc";
 import { qrcode } from 'vite-plugin-qrcode';
 
 export default defineConfig({
     plugins: [react(), qrcode()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
+    }
 });
