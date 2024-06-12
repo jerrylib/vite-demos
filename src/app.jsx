a809ee5c9ab5121bb4e226348801123e730da22f
韩旭gogogo <js_bin@163.com>
2024-06-12 17:44:05 +0800

    feat: routers update

--- src/app.jsx
+++ src/app.jsx
@@ -1,22 +1,21 @@
 import React from 'react';
 import { createRoot } from "react-dom/client"
 
-import {
-    createBrowserRouter,
-    RouterProvider,
-} from "react-router-dom";
+// import {
+//     createBrowserRouter,
+//     RouterProvider,
+// } from "react-router-dom";
 
 // === Components === //
 import Header from '@/components/Header'
 
 // === Routers === //
-import routers from '@/routers'
+// import routers from '@/routers'
+import { Routes } from '@generouted/react-router'
 
-const router = createBrowserRouter(routers);
+// const router = createBrowserRouter(routers);
 
-const root = createRoot(document.querySelector('#app'))
-root.render(<React.StrictMode>
+createRoot(document.getElementById('app')).render(<React.StrictMode>
     <Header />
-    <RouterProvider router={router} fallbackElement={<p>Loading...</p>} >
-    </RouterProvider>
+    <Routes />
 </React.StrictMode >)
--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,10 @@ import * as path from 'path'
 // === Plugins === //
 import react from "@vitejs/plugin-react-swc";
 import { qrcode } from 'vite-plugin-qrcode';
+import generouted from '@generouted/react-router/plugin'
 
 export default defineConfig({
-    plugins: [react(), qrcode()],
+    plugins: [react(), qrcode(), generouted()],
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
